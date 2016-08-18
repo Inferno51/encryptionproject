e2097bc3b324d686910664621101039fc7223bf9
Jon Pack <jon.pack@me.com>
2016-08-18 15:34:41 -0400

    Updated code with user prompts

--- src/main/java/com/encryptionproject/Encrypt.java
+++ src/main/java/com/encryptionproject/Encrypt.java
@@ -1,6 +1,47 @@
 package com.encryptionproject;
 
+import java.util.Scanner;
+
 public class Encrypt {
+	
+	public static void main(String[] args) {
+		userInputMethod();
+	}
+
+	public static void userInputMethod() {
+		Scanner sc = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		String userInput = null;
+		int userKey = 0;
+		
+		System.out.println("Welcome to the Encryption App!");
+		System.out.println("Please enter a word to encrypt:\n");
+		userInput = sc.nextLine();
+		
+		System.out.println("Please enter a key for your encryption: Enter a number between 1 and 5.");
+		userKey = sc2.nextInt();
+		
+		System.out.println(encryptionizer(userInput, userKey));
+		
+		System.out.println("Do you want to decrypt a word? Enter yes or no:");
+		userInput = sc.nextLine();
+		
+		if (userInput.equalsIgnoreCase("yes")){
+			System.out.println("Please enter a word to decrypt:\n");
+			userInput = sc.nextLine();
+			
+			System.out.println("Enter the key you used to encrypt your word: Enter a number between 1 and 5.");
+			userKey = sc2.nextInt();
+			
+			System.out.println(Decrypt.decryptionizer(userInput, userKey));
+		} else if (userInput.equalsIgnoreCase("no")) {
+			System.exit(0);
+		}
+		
+		sc.close();
+		sc2.close();
+	}
+	
 	protected static String encryptionizer(String encryptThis, int key) {
 		int stringLength = encryptThis.length();
 		String encrypted = "";
